dbf2970d3ed73b3e727f449a89d3625bc4a7f607
123Vlad12 <Belialus@mail.ru>
2022-11-25 16:49:33 +0300

    add hw_2

---
+++ Bobkov-hw_2/script.js
@@ -0,0 +1,59 @@
+// 1
+
+let x = 20;
+let y = 58;
+let z = 42;
+let Task1 = x + y + z;
+console.log(Task1);
+
+// 2
+
+let second = 60;
+let minutes = 60;
+let hour = 24;
+let year = 365;
+let myAge = 27;
+let mySeconds = year * myAge * hour * minutes * second;
+console.log(mySeconds);
+
+// 3
+
+let count = 42;
+let userName = "42";
+count = "42";
+userName = 42;
+console.log(typeof "count");
+console.log(typeof userName);
+
+// 4
+
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+a = "1";
+b = "2";
+c = "белых медведей";
+let Task4 = a + b + c;
+console.log(Task4);
+
+// 5
+
+let Task5 = доступ;
+let marine = морпех;
+let frost = наледь;
+let reproach = попрек;
+let chopped = рубило;
+let lengthWords = Task5 + marine + frost + reproach + chopped;
+console.log(lengthWords);
+
+// 6
+
+// 7
+
+let Task7 = prompt("Ваше имя");
+let age = prompt("Ваши вазраст");
+alert("Как вас зовут");
+console.log(Task7);
+console.log(age);
+
+// 8
