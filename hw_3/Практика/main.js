ec9e8d3c93ce75cea12b0ad621096dc7ffea81d8
Hanna Burkovskaya <hanna.burkovskaya@gmail.com>
2022-11-29 21:31:51 +0300

    add prakt

---
+++ hw_3/Практика/main.js
@@ -0,0 +1,71 @@
+// let a = prompt("Введите число");
+// len = a.length;
+// console.log(len);
+// a = Number(a);
+
+// a1 = Math.floor(a / 100);
+// a2 = Math.floor((a-(a1*100)) / 10);
+// a3 = (a-(a1*100)-(a2*10));
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a1+a2+a3);
+
+// let a = +prompt("Введите число");
+// for(let i = 1; i <= a; i++) {
+//     console.log(`${i}sheep...`);
+// }
+
+// let width = +prompt("Введите ширину");
+// let length = prompt("Введите высоту");
+
+// if (length === width) {
+//     console.log(`Square = ${width**2}`)
+// } else  {
+//     console.log(`Perimetr = ${(width*2)+(length*2)}`)
+// }
+
+// let num = +prompt("Введите число");
+// let str = prompt("Введите строку");
+
+// console.log(str.repeat(num));
+
+// let num1 = +prompt("Введите число");
+// let str = prompt("+ - * /");
+// let num2 = +prompt("Введите число");
+
+// switch (str){
+//     case '+': 
+//     console.log (num1 + num2);
+//     break;
+//     case '-': 
+//     console.log (num1 - num2);
+//     break;
+//     case '*': 
+//     console.log (num1 * num2);
+//     break;
+//     case '/': 
+//     console.log (num1 / num2);
+//     break;
+//     default: 
+//     alert("Ошибка!!")
+// }
+
+// let str = prompt("Как вас зовут");
+// if (str ==  "Джонни") {
+//     alert("Люблю тебя!")
+// } else {
+//     alert(`Привет, ${str}`)
+// }
+
+
+// let a = +prompt('Введите число');
+// if (a%2 == 0) {
+//     for(let i = 0; i <= a; i++){
+//         console.log(i)
+//     }
+// } else {
+//     for(let j = a; j >= 0; j--){
+//         console.log(j)
+//     }
+// }
