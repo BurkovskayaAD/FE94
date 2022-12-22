f722bcc8bd983b4bfc7c08c7632a6ef53970bfb6
endsnipes <115029557+endsnipes@users.noreply.github.com>
2022-12-22 17:52:47 +0300

    init(hw2):done

---
+++ hw_2.js
@@ -0,0 +1,49 @@
+//Task 1
+let x = 20
+let y = 58
+let z = 42
+console.log(x+y+z)
+//  TASK 2
+let a = 23
+let b = 60
+let c = 60
+let d = 24
+let e = 365
+myAgeInSeconds =a*b*c*d*e 
+console.log(myAgeInSeconds)
+// TASK 3
+let count = 42;
+let userName = "42";
+console.log(typeof "count");
+console.log(typeof userName);
+count = "42";
+userName = 42;
+// TASK 4
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+a="1"
+b="2"
+let d = a + b + c;
+console.log(d);
+// TASK 5
+let a = "доступ"
+let b = "морпех"
+let c = "наледь"
+let d= "попрек"
+let e= "рубило"
+let length = (a+b+c+d+e).length
+console.log(length)
+// TASK 6
+let a = "aaa";
+let b = 123;
+let c = true;
+let typea = typeof a;
+let typeb = typeof b;
+let typec = typeof c;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+// TASK 7
+let a = prompt("Ваше имя и возраст")
+console.log(a)
